88581eb8e402029dee98c8c286570af533f27070
Ibtsam Ahmad <ahmadibtsam07@gmail.com>
2022-02-08 18:38:56 +0500

    updated settings

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -12,7 +12,7 @@ if READ_DOT_ENV_FILE:
 
 DEBUG = env('DEBUG')
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY',"default_value")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
